4675be00fa24195c91b486efef383ac5311ffa47
David Chen <davidc3287@gmail.com>
2021-01-15 11:33:40 -0500

    style and finish basic elements

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
-import {Button, Container, Grid, Header, Input} from 'semantic-ui-react'
+import {Button, Container, Grid, Header, Input, Label, Popup} from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
 
 function App() {
   return (
     <Container>
-      <Header>Reddit Username Generator</Header>
+      <Header size='huge' style={{paddingTop: '20px'}}>Reddit Username Generator</Header>
       <Grid columns={3}>
         <Grid.Column>
-          <Input focus placeholder='8'></Input>
+          <Popup 
+            content='Generate this many usernames.'
+            trigger={
+              <Input label='Number of usernames:' placeholder='20'></Input>
+            }
+            on='focus'
+          />
         </Grid.Column>
         <Grid.Column>
-          <Input focus placeholder='11'></Input>
+          <Popup 
+            content='Temperatures are between 0 and 1, with lower temperatures producing more predictable text and higher temperatures resulting in more surprising text.'
+            trigger={
+              <Input label='Temperature:' placeholder='0.5'></Input>
+            }
+            on='focus'
+          />
         </Grid.Column>
         <Grid.Column>
-          <Input focus placeholder='12'></Input>
+          <Popup 
+            content='Start string (ex: PM-ME). This will be used as the prefix for the first username generated. If left blank, the first username will start with a random character.'
+            trigger={
+              <Input label='Start string:'></Input>
+            }
+            on='focus'
+          />
         </Grid.Column>
       </Grid>
     </Container>
