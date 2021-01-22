649271258e96d81a59a03eab29ca5dd067cf6bf4
David Chen <davidc3287@gmail.com>
2021-01-22 11:07:13 -0500

    add cors

--- api/api.py
+++ api/api.py
@@ -1,8 +1,12 @@
 import time
 from flask import Flask
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
+
 
 @app.route('/time')
+@cross_origin()
 def get_current_time():
     return {'time': time.time()}
